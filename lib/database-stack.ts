77ff6065c48ed03409a272880b80970fc37894e2
RachelRusiecki <rachrusiecki@gmail.com>
2025-07-26 15:01:03 -0500

    feat: update database stack to use AWS Secrets Manager

--- lib/database-stack.ts
+++ lib/database-stack.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as secretsManager from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
 import { DatabaseCluster } from "aws-cdk-lib/aws-docdb";
 
@@ -14,17 +15,27 @@ interface DatabaseStackProps extends cdk.StackProps {
 export class DatabaseStack extends cdk.Stack {
   public readonly cluster: DatabaseCluster;
   public readonly clusterEndpoint: string;
+  public readonly secret: secretsManager.Secret;
 
   constructor(scope: Construct, id: string, props: DatabaseStackProps) {
     super(scope, id, props);
 
+    this.secret = new secretsManager.Secret(this, "DatabaseSecret", {
+      description: "Pendulum DocumentDB cluster credentials",
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: process.env.DB_USER || "admin"
+        }),
+        generateStringKey: "password",
+        excludeCharacters: '"@/\\',
+        passwordLength: 16,
+      },
+    });
+
     this.cluster = new DatabaseCluster(this, "Database", {
       masterUser: {
-        username: process.env.DB_USER || "admin",
-        // CHANGE BELOW TO USE AWS sECRETS MANAGER FOR PRODUCTION!!!
-        password: cdk.SecretValue.unsafePlainText(
-          process.env.DB_PW || "password123",
-        ),
+        username: this.secret.secretValueFromJson("username").unsafeUnwrap(),
+        password: this.secret.secretValueFromJson("password"),
       },
       instanceType: ec2.InstanceType.of(
         ec2.InstanceClass.T3,
