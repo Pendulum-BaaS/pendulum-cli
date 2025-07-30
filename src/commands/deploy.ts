831208dcd1b374d10de35e84ef7c1a86d1ce0b35
RachelRusiecki <rachrusiecki@gmail.com>
2025-07-29 20:56:52 -0500

    refactor: extract common AWS functionality to common function

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -4,6 +4,7 @@ import ora from "ora";
 import { resolve } from "path";
 import { runCommand } from "../utils/runCommand";
 import { checkAWSConfiguration } from "../utils/checkAWSConfiguration";
+import { getAWSConfiguration } from "../utils/getAWSConfiguration";
 
 async function installCDKDependencies(cliPath: string) {
   const spinner = ora("Intalling CDK dependencies...").start();
@@ -107,70 +108,7 @@ export async function DeployCommand() {
     return;
   }
 
-  const { awsAccountId, awsRegion } = await inquirer.prompt([
-    {
-      type: "input",
-      name: "awsAccountId",
-      message: "Enter your AWS Account ID:",
-      validate: (input: string) => {
-        if (!input || input.trim().length === 0) {
-          return "AWS Account ID is required";
-        } else if (!/^\d{12}$/.test(input.trim())) { // checks for exactly 12 digits
-          return "AWS Account ID must be exactly 12 digits";
-        } else {
-          return true;
-        }
-      },
-    },
-    {
-      type: "list",
-      name: "awsRegion",
-      message: "Select your AWS region:",
-      choices: [
-        {
-          name: "US East (N. Virginia) - us-east-1",
-          value: "us-east-1",
-        },
-        {
-          name: "US East (Ohio) - us-east-2",
-          value: "us-east-2",
-        },
-        {
-          name: "US West (Oregon) - us-west-2",
-          value: "us-west-2",
-        },
-        {
-          name: "US West (N. California) - us-west-1",
-          value: "us-west-1",
-        },
-        {
-          name: "Europe (Ireland) - eu-west-1",
-          value: "eu-west-1",
-        },
-        {
-          name: "Europe (London) - eu-west-2",
-          value: "eu-west-2",
-        },
-        {
-          name: "Europe (Frankfurt) - eu-central-1",
-          value: "eu-central-1",
-        },
-        {
-          name: "Asia Pacific (Sydney) - ap-southeast-2",
-          value: "ap-southeast-2"
-        },
-        {
-          name: "Asia Pacific (Tokyo) - ap-northeast-1",
-          value: "ap-northeast-1",
-        },
-        {
-          name: "Asia Pacific (Singapore) - ap-southeast-1",
-          value: "ap-southeast-1",
-        },
-      ],
-      default: "us-east-1",
-    },
-  ]);
+  const { awsAccountId, awsRegion } = await getAWSConfiguration();
 
   const { confirmDeployment: finalConfirm } = await inquirer.prompt([
     {
